778e31d57a1cb69301fec53e43b513713dd18430
willer-barros <willerbarros137@gmail.com>
2026-07-10 15:52:35 -0300

    feat routes PUT and DELETE

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Api Para os alunos praticarem o relacionamento 1:n
 
+###📋 Estrutura do Projeto
+Modelos de Dados
+O schema define dois modelos principais com relacionamento um-para-muitos:
+
+Farm: Representa uma fazenda com nome e localização
+Plot: Representa talhões de cultivo vinculados a uma fazenda, com tipo de cultura e área em hectares 
--- src/server.js
+++ src/server.js
@@ -64,6 +64,60 @@ app.get('/farms', async (req, res) => {
   }
 });
 
+app.put('/farms/:id', async (req, res) =>{
+
+    try{
+    const {id} = req.params;
+    const {name, location } = req.body
+
+    if (name != undefined && name.trim() === ""){
+        return res.status(400).json({erro: "o nome da fazendo nao pode ser vazia"})
+    }
+    const farmExists = await prisma.farm.findUnique({
+        where: {id: Number(id)}
+
+    })
+
+    if(!farmExists){
+        return res.status(404).json({erro: "fazenda nao encontrada para atualizar"})
+    }
+
+    const farmUpdate = await prisma.farm.update({
+        where: {id: Number(id)},
+        data: {
+            name,
+            location
+        }
+    })
+
+    return res.json(farmUpdate)
+} catch(error){
+    return res.status(500).json({erro: "Erro ao atualizar a fazenda"})
+}
+})
+
+app.delete('/api/farms/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const farmExistente = await prisma.farm.findUnique({
+      where: { id: Number(id) }
+    });
+
+    if (!farmExistente) {
+      return res.status(404).json({ erro: "Fazenda não encontrada para exclusão." });
+    }
+
+    await prisma.farm.delete({
+      where: { id: Number(id) }
+    });
+
+    return res.json({ mensagem: "Fazenda e todos os seus talhões associados foram excluídos com sucesso." });
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro ao excluir a fazenda." });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`🚜🟢Agrotech rodante com sucesso`);
 });
